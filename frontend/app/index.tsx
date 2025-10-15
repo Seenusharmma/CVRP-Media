4a05bb32f565fe82f6cc9dd542a75dfa11156cd6
Seenusharma <roshansharma7250@gmail.com>
2025-10-15 21:35:20 +0530

    start creating Mobile Ui

---
+++ frontend/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="text-red-600 items-center flex justify-center text-2xl">
+      <Text className="text-red-600 items-center flex justify-center text-2xl">Welocme</Text>
+    </View>
+  );
+}
---
+++ frontend/hooks/useSocialAuth.ts
@@ -0,0 +1,24 @@
+import { useSSO } from "@clerk/clerk-expo";
+import { Alert } from "react-native";
+import { useState } from "react"
+
+export const useSocialAuth = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const { startSSOFlow} = useSSO();
+
+    const handleSocialAuth = async (strategy: "oauth_google" | "oauth_apple") => {
+        setIsLoading(true);
+        try {
+            const {createdSessionId, setActive} = await startSSOFlow({strategy});
+
+            if (createdSessionId && setActive) {
+                await setActive({session: createdSessionId})
+            }
+        } catch (error) {
+            console.log("Error in social auth", error);
+           const provider = strategy == "oauth_google" ? "Google" : "Apple"
+           Alert.alert("Error", `Failed  to sign in with ${provider} Please try again`)
+            
+        }
+    }
+}
